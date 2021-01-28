00907fd7758d15019f70491fd5fa98d2e85abc21
Stefan Probst <stefan.probst@cbc-extern.de>
2021-01-28 10:35:25 +0100

    cleanup

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,6 @@
-import message from '../index';
 
 it('Works', () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  expect(message).toBe('This module must not be required!');
+  expect('This module must not be required!').toBe('This module must not be required!');
 });
